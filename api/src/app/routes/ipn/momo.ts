e08d9fd5f67334d695d37d00dcbc5472242eb24c
doai <doai.nguyen@sotatek.com>
2025-03-14 18:26:38 +0700

    feat: add momo webhook

--- api/src/app/routes/ipn/momo.ts
+++ api/src/app/routes/ipn/momo.ts
@@ -3,9 +3,10 @@ import { loadDonations } from '../../shared/hepler';
 
 export default async function (fastify: FastifyInstance) {
   fastify.get('/momo', async function (request, reply) {
-    return reply.status(200).code(200).send({
-      message: 'from momo'
-    });
+    return reply.status(200).code(204);
+  });
+  fastify.post('/momo', async function (request, reply) {
+    console.log(`{} payload: `, request.body);
+    return reply.code(204).send();
   });
 }
-
